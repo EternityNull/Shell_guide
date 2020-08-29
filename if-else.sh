425d839229ecd62c3a1bc70f6019d37676a093c1
eternitynull <eternitynull@outlook.com>
2020-08-30 00:13:49 +0800

    拆分文件

---
+++ if-else.sh
@@ -0,0 +1,10 @@
+var=5
+# if [[ $var == 5 ]]
+if [[ $var -eq 5 ]]
+then 
+	echo "true"
+else
+	echo "false"
+fi
+
+[[]] 比 [] 更强大，[]与test等价 只能做简单判断
---
+++ loop.sh
@@ -0,0 +1,39 @@
+# arr=(1 2 3)
+# for i in ${arr[@]}
+# do
+# #	echo $i
+# 	echo $(($i + 1 * 2))
+# done
+
+# for i in 1 2
+# do
+# 	echo $i
+# done
+
+# for file in `ls`;do echo $file; done
+
+# max_len=9
+# for ((i=0; i<$max_len; i++))
+# do
+# 	let i++
+# 	echo $i
+# done
+
+# i=2
+# while ((i < 5))
+# do
+# 	echo $i
+# 	let i++
+# done
+
+# i=2
+# # while [[ $i -lt 10]]
+# while :
+# do
+# 	echo $i
+# 	let i=i+1
+# 	if [[ $i == 10 ]]
+# 	then
+# 		break
+# 	fi
+# done
